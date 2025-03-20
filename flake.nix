f5e6a37a42591bc1815f5bb746a574077e929038
Adrian Gierakowski <agierakowski@gmail.com>
2025-03-20 20:31:35 +0000

    add treefmt-nix with dprtin to format ts files

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?rev=95ea544c84ebed84a31896b0ecea2570e5e0e236";
     nixpkgs-latest.url = "github:nixos/nixpkgs?rev=c80f6a7e10b39afcc1894e02ef785b1ad0b0d7e5";
-    utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs-latest";
+    utils.url = "github:numtide/flake-utils?rev=6ee9ebb6b1ee695d2cacc4faa053a7b9baa76817";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -16,6 +18,7 @@
       nixpkgs,
       nixpkgs-latest,
       utils,
+      treefmt-nix,
       ...
     }:
     let
@@ -39,6 +42,56 @@
           inherit system;
         };
         inherit (pkgs) lib;
+        treefmt-eval = (import treefmt-nix).evalModule pkgs {
+          programs.nixfmt.enable = true;
+          # programs.nixfmt.srict = true;
+          programs.dprint.enable = true;
+          programs.dprint = {
+            includes = lib.mkForce [
+              "**/*.ts"
+            ];
+          };
+          programs.dprint.settings = {
+            plugins = [
+              "https://plugins.dprint.dev/typescript-0.94.0.wasm"
+            ];
+            indentWidth = 2;
+            lineWidth = 100;
+            incremental = true;
+            useTabs = false;
+            typescript = {
+              "arrowFunction.useParentheses" = "preferNone";
+              "binaryExpression.linePerExpression" = true;
+              "enumDeclaration.memberSpacing" = "newLine";
+              "jsx.quoteStyle" = "preferSingle";
+              lineWidth = 80;
+              "memberExpression.linePerExpression" = true;
+              nextControlFlowPosition = "sameLine";
+              quoteProps = "asNeeded";
+              quoteStyle = "preferSingle";
+              semiColons = "asi";
+            };
+          };
+          settings.formatter = {
+            nixfmt = {
+              excludes = [
+                "yarn-manifest.nix"
+              ];
+              includes = lib.mkForce [
+                "*.nix"
+              ];
+            };
+            dprint = {
+              includes = lib.mkForce [
+                "*.ts"
+              ];
+            };
+          };
+          settings.global.on-unmatched = "info";
+          settings.global.excludes = [
+            ".yarn/*"
+          ];
+        };
       in
       {
         packages = rec {
@@ -58,6 +111,7 @@
               yarn up -E @yarnpkg/cli @yarnpkg/core @yarnpkg/fslib @yarnpkg/libzip @yarnpkg/plugin-file @yarnpkg/plugin-pnp @yarnpkg/pnp @yarnpkg/builder
             '';
           };
+
           tests = {
             patch =
               let
@@ -78,17 +132,14 @@
               '';
           };
         };
+        treefmt-config = treefmt-eval.config;
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
               nodejs
               yarnBerry
               pkgs-latest.nixfmt-rfc-style
-              (pkgs-latest.nixfmt-tree.override {
-                settings.formatter.nixfmt.excludes = [
-                  "yarn-manifest.nix"
-                ];
-              })
+              treefmt-eval.config.build.wrapper
             ];
           };
           tests-patch = pkgs.mkShell {
