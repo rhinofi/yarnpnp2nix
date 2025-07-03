66d655be360188c38a2c08163504d6c405357396
Adrian Gierakowski <agierakowski@gmail.com>
2025-07-03 13:05:54 +0200

    ci effect: use nix_26

--- flake.nix
+++ flake.nix
@@ -190,7 +190,7 @@
           in
           effectLib.mkEffect {
             effectScript = pkgs.lib.getExe self.packages.x86_64-linux.yarnpnp2nix-test;
-            inputs = with pkgs; [ nixVersions.nix_2_29 ];
+            inputs = with pkgs; [ nixVersions.nix_2_26 ];
             env.NIX_CONFIG = "extra-experimental-features = nix-command flakes";
             env.ROOT = "${self}";
           };
